9ec3b3fb85bc75cda86c2681cd364005bc96be10
Garrett Skjelstad <garrettskjelstad@Garretts-MacBook-Pro-2.local>
2024-12-20 05:53:52 -0800

    another go

--- jobs/location_data.py
+++ jobs/location_data.py
@@ -78,7 +78,7 @@ class SiteImportJob(Job):
     inputfile = FileVar(required=True)
  
     def run(self, *args, **kwargs):
-        with open(self.inputfile, "r") as csvfile:
+        with open(self.kwargs["inputfile"], "r") as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 normalized_row = self.normalize_data(row)
