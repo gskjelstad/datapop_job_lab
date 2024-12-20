6e1bb8432fbe9dc239bff8f9865efd7212220fcc
Garrett Skjelstad <garrettskjelstad@Garretts-MacBook-Pro-2.local>
2024-12-20 06:36:13 -0800

    again

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 """Import Jobs."""
 
-from .jobs.location_data import SiteImportJob
-from nautobot.core.celery import register_jobs
+# from .jobs.location_data import SiteImportJob
+# from nautobot.core.celery import register_jobs
 
-register_jobs(SiteImportJob)
+# register_jobs(SiteImportJob)
