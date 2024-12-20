4baaea27c2cb3cc698156dd1d11f142bbb9b3ae4
Garrett Skjelstad <garrettskjelstad@Garretts-MacBook-Pro-2.local>
2024-12-20 06:09:06 -0800

    another go

--- jobs/location_data.py
+++ jobs/location_data.py
@@ -77,7 +77,7 @@ class SiteImportJob(Job):
         
     file = FileVar(required=True)
  
-    def run(self, *args, **kwargs):
+    def load_source_data(self):
         _file = self.kwargs["file"].read().decode("utf-8-sig")
         with open(_file, "r") as csvfile:
             reader = csv.DictReader(csvfile)
@@ -109,4 +109,4 @@ class SiteImportJob(Job):
         return row
 
 
-register_jobs(SiteImportJob)
+#register_jobs(SiteImportJob)
